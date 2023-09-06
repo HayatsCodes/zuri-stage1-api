647c9528de8c51870deb620f0937ae7532fec8c9
Hayats <hayatudeenabdulrahman@gmail.com>
2023-09-07 00:51:06 +0100

    created a simple api endpoint

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.get('/api', (req, res) => {
+    
+    const currentDate = new Date();
+
+    const {slack_name, track} = req.query;
+    const current_day = daysOfWeek[currentDate.getDay()];
+    const utc_time = new Date(currentDate.getTime() + 2000);
+    const github_file_url = 'https://github.com/hayatscodes/zuri-stage1-api/blob/main/file_name.ext';
+    const github_repo_url = 'https://github.com/username/zuri-stage1-api';
+    const status_code = 200;
+
+    res.status(200).json(
+        {
+            slack_name,
+            current_day,
+            utc_time,
+            track,
+            github_file_url,
+            github_repo_url,
+            status_code
+        }
+    )
+});
+
+app.listen(3000, () => {
+    console.log('App started on port 3000');
+})
